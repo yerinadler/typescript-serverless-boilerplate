b45c6b7c4c772002dbf9d8cead1da7f2391cd2e7
Yerin J. Adler <yerin.adler@gmail.com>
2021-07-11 18:41:15 +0700

    Revise error handling middleware

--- src/interfaces/middlewares/error.middleware.ts
+++ src/interfaces/middlewares/error.middleware.ts
@@ -1,13 +1,24 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import middy from '@middy/core';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+
 export const errorHandlingMiddleware = () => {
-  return {
-    onError: ({ error: { statusCode = 500, message = 'Internal server error' } }: any) => {
-      return {
-        statusCode,
-        body: JSON.stringify({ statusCode, message })
-      };
-    }
+  const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = (request: any) => {
+    const {
+      error: {
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR,
+        message = getReasonPhrase(500)
+      }
+    } = request;
+    
+    request.response = {
+      statusCode,
+      body: JSON.stringify({ statusCode, message })
+    };
   };
+  
+  return { onError };
 };
